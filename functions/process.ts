df77b0d90eba529aa47c6c930a420cfea284729a
Yuan Huang <huangyuan3h@gmail.com>
2024-08-15 14:46:58 +0800

    Revert "updat ethe header"
    This reverts commit 7713ea26f5399d9f46e2e6e68373319b8f651fbf.

--- functions/process.ts
+++ functions/process.ts
@@ -111,22 +111,12 @@ async function fetchWithTimeout(
   options.headers = {
     "User-Agent":
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    Accept:
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Cache-Control": "no-cache",
     Connection: "keep-alive",
-    accept:
-      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "accept-language":
-      "en-GB,en;q=0.9,en-US;q=0.8,zh-CN;q=0.7,zh;q=0.6,ja;q=0.5",
-    "cache-control": "max-age=0",
-    priority: "u=0, i",
-    "sec-ch-ua":
-      '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": '"macOS"',
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "none",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1",
+    "Upgrade-Insecure-Requests": "1",
     ...options.headers, // 合并用户自定义 headers
   };
 
